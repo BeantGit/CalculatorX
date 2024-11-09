ab6808f84d24c22b900ab6e698f142ef811688d9
BeantGit <er.beantsaini@gmail.com>
2024-11-09 23:44:25 +0530

    Removed Addition\Subtraction funtionality.

--- project_x/CalcX/src/MathOperations.java
+++ project_x/CalcX/src/MathOperations.java
@@ -1,12 +1,9 @@
 import org.math.basic.*;
 public class MathOperations {
-	private Addition a = new Addition();
-	private Subtraction s = new Subtraction();
 	
-	
-	public void add(int a, int b)
+	public int add(int a, int b)
 	{
-		//System.out.println("Addition is : "+a.add(a,b));
+		return a - b;
 	}
 	
 	
