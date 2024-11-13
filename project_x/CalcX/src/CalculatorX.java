6d5af8598c121fbbbff476f26e9756dfcbaae16e
BeantGit <er.beantsaini@gmail.com>
2024-11-14 00:32:00 +0530

    added subtraction function in MathOperations.

--- project_x/CalcX/src/CalculatorX.java
+++ project_x/CalcX/src/CalculatorX.java
@@ -4,6 +4,9 @@ public class CalculatorX {
 	public static void main(String[] args) {
 		System.out.println("*** Calculator ****");
 		MathOperations opr = new MathOperations();
+		opr.add(100,100);
+		opr.sub(200,100);
+
 	}
 
 }
--- project_x/CalcX/src/MathOperations.java
+++ project_x/CalcX/src/MathOperations.java
@@ -1,7 +1,13 @@
+
+# change-001 BS added subtraction function.
 import org.math.basic.*;
 public class MathOperations {
 	
 	public int add(int a, int b)
+	{
+		return a + b;
+	}
+	public int sub(int a,int b)
 	{
 		return a - b;
 	}
